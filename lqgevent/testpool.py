b1339d89374de41c97919ded1ac43891c2cde432
bynoting <bynoting@21cn.com>
2016-01-06 21:56:44 +0800

    1

---
+++ lqgevent/testpool.py
@@ -0,0 +1,20 @@
+# coding=gbk
+import gevent
+from gevent.queue import Queue
+from gevent.pool import Pool
+from gevent import getcurrent
+
+def DoSomething():
+	print "thread %s " % id(getcurrent())
+	gevent.sleep(3)
+
+# 本测试发现：pool中add 后超出size 即会开始执行，可以看做pool size +1 的容量大小
+# greenlet 对象在推拉窗模式中 可以复用
+pool = Pool(2) # 可并行个任务
+print pool.free_count()
+# pool.add(gevent.spawn(DoSomething))
+
+# print "stage"
+for i in range(10):
+	pool.add(gevent.spawn(DoSomething))
+pool.join()
