8814331f6c459e70a06dc40e6a299eccc45dee4e
bynoting <bynoting@21cn.com>
2016-01-21 21:09:11 +0800

    1

---
+++ lqspider/yaohao/yaohao/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for yaohao project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'yaohao'
+
+SPIDER_MODULES = ['yaohao.spiders']
+NEWSPIDER_MODULE = 'yaohao.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'yaohao (+http://www.yourdomain.com)'
---
+++ lqspider/yaohao/yaohao/spiders/myspider.py
@@ -0,0 +1,31 @@
+# coding=gbk
+from scrapy.selector import Selector
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor as sle
+
+class MySpider(CrawlSpider):
+    name = "yaohao"
+    allowed_domains = ["bjhjyd.gov.cn"]
+    start_urls = [
+        "http://www.bjhjyd.gov.cn/",
+    ]
+    rules = [ # 定义爬取URL的规则
+              # 在起始页面的下一级进行查找
+        Rule(sle(allow=("https://www\.taobao\.com/market/.+"))),
+        Rule(sle(allow=("item\.taobao\.com/item\.htm\?spm=.+")), follow=True, callback='parse_item')
+    ]
+
+    def parse_start_url(self, response):
+        print response.url
+        sel = Selector(response)
+        print "+++++++++++++++++++++++"
+        print sel.xpath('//*[@id="getValidCode"]/img)').extract()
+        return None
+
+    def parse_item(self, response):
+        items = []
+    # // filename = response.url.split("/")[-2]
+        print "taobao =========================%s"%response.url
+        return items
+
+
