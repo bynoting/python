79301a89863dcbbc04957e77b26f8456e82175e7
liqiangpi <bynoting@21cn.com>
2016-12-22 20:03:55 +0800

    .ropeproject/ 	.volumestatics.py.swp

--- volumestatics.py
+++ volumestatics.py
@@ -4,9 +4,17 @@ import os
 def Statistics(aRoot):
     vloume_limit = 200  #200M
     for rootpath,dirname,filename in os.walk(aRoot):
-        os.path.getsize
+        for file in filename:
+            filesize  = round((os.path.getsize(file)/1024/1024),4)
+            print "filename is "+ file + " filesize is " + str(filesize)
+       # if filesize > 500*1000
 
 if __name__ =="__main__":
     import traceback
     try:
+        Statistics(os.path.curdir)
+    except Exception, e:
+        print e.message
+
+
 
