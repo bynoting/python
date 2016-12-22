abba93d94c91386eebb48cd4bdf35117a9954fc0
liqiangpi <bynoting@21cn.com>
2016-12-22 19:20:21 +0800

    新文件：   volumestatics.py

---
+++ volumestatics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+
+def Statistics(aRoot):
+    vloume_limit = 200  #200M
+    for rootpath,dirname,filename in os.walk(aRoot):
+        os.path.getsize
+
+if __name__ =="__main__":
+    import traceback
+    try:
+
