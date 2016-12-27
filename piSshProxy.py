a6bb01945a80d51ae59f5952e4e22c1d7041d392
bynoting <bynoting@21cn.com>
2016-12-27 21:36:19 +0800

    1

--- piSshProxy.py
+++ piSshProxy.py
@@ -4,8 +4,8 @@ import subprocess
 def runSSh():
 
     #ssh -t -D 0.0.0.0:9999 585c9c8e2d527108680000dc@wp-maodou.rhcloud.com
-    sshargs = "{cmd} {argTty} {argPort} {IP}:{Port} {sshKey}".\
-        format(cmd="ssh",argTty="-t",argPort="-D",IP="0.0.0.0",Port ="9999",sshKey="585c9c8e2d527108680000dc@wp-maodou.rhcloud.com")
+    sshargs = ("{cmd} {argTty} {argPort} {IP}:{Port} {sshKey}".\
+        format(cmd="ssh",argTty="-t",argPort="-D",IP="0.0.0.0",Port ="9999",sshKey="585c9c8e2d527108680000dc@wp-maodou.rhcloud.com") )
     o = subprocess.Popen(sshargs,stdout=subprocess.PIPE,shell= False)
     l = o.stdout.readline()
     while(l):
