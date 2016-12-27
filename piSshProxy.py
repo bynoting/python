d2cdb8001a8c3ddb677a075d7d6353ef23180d78
liqiangpi <bynoting@21cn.com>
2016-12-27 23:54:10 +0800

    修改：     piSshProxy.py

--- piSshProxy.py
+++ piSshProxy.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 import subprocess
+import shlex
 
 def runSSh():
 
     #ssh -t -D 0.0.0.0:9999 585c9c8e2d527108680000dc@wp-maodou.rhcloud.com
-    sshargs = "{cmd} {argTty} {argPort} {IP}:{Port} {sshKey}".\
-        format(cmd="ssh",argTty="-t",argPort="-D",IP="0.0.0.0",Port ="9999",sshKey="585c9c8e2d527108680000dc@wp-maodou.rhcloud.com")
+    args ="{cmd} {argTty} {argPort} {IP}:{Port} {sshKey}".format(cmd="ssh",argTty="-t",argPort="-D",IP="0.0.0.0",Port ="9999",sshKey="585c9c8e2d527108680000dc@wp-maodou.rhcloud.com")
+    print args
+    sshargs = shlex.split(  args)
     o = subprocess.Popen(sshargs,stdout=subprocess.PIPE,shell= False)
     l = o.stdout.readline()
     while(l):
         print l
-    runSSh()
 
 runSSh()
