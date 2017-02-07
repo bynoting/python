17787064ed20236db34852c6e2473052b5dd4a75
bynoting <bynoting@21cn.com>
2017-02-07 15:34:44 +0800

    1

--- OperaPexpectDemo.py
+++ OperaPexpectDemo.py
@@ -3,7 +3,7 @@ import sys
 import getpass
 
 def main():
-    demo1()
+    demo2()
 
 def demo1():
     try:
@@ -13,6 +13,13 @@ def demo1():
         print "except !!!!!!!!!!!!!!!" + e.message
     finally:
         print ">>>>>>>>>>>>>>>>>>>>>>>>end"
+def demo2():
 
+    try:
+        child = pexpect.spawn("sudo lsof -i:9999")
+        child.expect(pexpect.EOF)
+        print child.before
+    finally:
+        print "end"
 if __name__ == "__main__":
     main()
